6bc145d797826aac644951239c268a0989c14ca0
Mehedi Hasan <51486542+mehediishere@users.noreply.github.com>
2023-01-10 00:32:01 +0600

    ---

--- part/sidebar.php
+++ part/sidebar.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a href="index.php" class="brand-link">
-          <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
+          <img src="dist/img/webtouch.png" alt="Logo" class="brand-image img-circle elevation-3" style="opacity: 0.8" />
           <span class="brand-text font-weight-light text-uppercase">Pharmacy</span>
         </a>
 
@@ -10,7 +10,7 @@
           <!-- Sidebar user panel (optional) -->
           <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
-              <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image" />
+              <img src="dist/img/user1.jpg" class="img-circle elevation-2" alt="User Image" />
             </div>
             <div class="info">
               <a href="#" class="d-block">
